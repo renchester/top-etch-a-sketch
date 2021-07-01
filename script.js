0b7122583eb4c9e333f45fe45f5ea2f3cb84409c
Renchester Ramos <renchesterjramos@gmail.com>
2021-07-01 23:24:55 +0800

    Add change color functionality

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ const queryInput = document.querySelector('.query-input');
 const warning = document.querySelector('.warning-container');
 
 const colorsContainer = document.querySelector('.colors-container');
-const pixelFillRainbow = document.querySelector('.pixel-fill-rainbow');
 
 const btnRandom = document.querySelector('.btn-random');
+const btnWarm = document.querySelector('.btn-warm');
+const btnCool = document.querySelector('.btn-cool');
 const btnGray = document.querySelector('.btn-gray');
-const btnPicker = document.querySelector('.btn-picker');
+const btnSpecify = document.querySelector('.btn-specify');
 
 const slider = document.querySelector('.slider');
 const btnSlider = document.querySelector('.btn-slider');
@@ -29,6 +30,7 @@ let brushState = true;
 let eraseState = false;
 
 let colorState = 'default';
+let fillColor = 'rgb(116, 116, 116)';
 
 // Functions
 function init(size = 16) {
@@ -73,16 +75,34 @@ function fillPixel(e) {
   if (!brushState) return;
   const pixel = e.target;
 
-  console.log(r.getProperty('--fill-color'));
-  eraseState
-    ? pixel.classList.remove('pixel-fill')
-    : pixel.classList.add('pixel-fill');
+  if (eraseState) {
+    pixel.style.backgroundColor = 'white';
+    return;
+  }
+
+  switch (colorState) {
+    case 'default':
+      pixel.style.backgroundColor = fillColor;
+      break;
+    case 'random':
+      pixel.style.backgroundColor = getRandomColor();
+      break;
+    case 'warm':
+      pixel.style.backgroundColor = getWarmColor();
+      break;
+    case 'cool':
+      pixel.style.backgroundColor = getCoolColor();
+      break;
+    case 'gray':
+      pixel.style.backgroundColor = getGrayColor();
+      break;
+  }
 }
 
 function erasePixel(e) {
   if (!brushState) return;
   const pixel = e.target;
-  pixel.classList.remove('pixel-fill');
+  pixel.style.backgroundColor = 'white';
 }
 
 function toggleErase(e) {
@@ -92,7 +112,7 @@ function toggleErase(e) {
 
 function eraseAllFills(e) {
   const pixels = document.querySelectorAll('.pixel');
-  pixels.forEach((pixel) => pixel.classList.remove('pixel-fill'));
+  pixels.forEach((pixel) => (pixel.style.backgroundColor = 'white'));
 }
 
 function toggleDrawing(e) {
@@ -104,47 +124,65 @@ function toggleDrawing(e) {
   btnToggle.textContent = brushState ? 'Stop drawing' : 'Start drawing';
 }
 
-function getRandomColor() {
+////// Color-related functions
+
+function changeColor(e) {
+  if (!e.target.classList.contains('btn')) return;
+  colorState = e.target.id.split('-').pop();
+
+  makeActive(e.target);
+}
+
+function getRandomColor(e) {
   const letters = '0123456789ABCDEF';
-  let color = '#';
+  let randomColor = '#';
   for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    randomColor += letters[Math.floor(Math.random() * 16)];
   }
-  return color;
+
+  return randomColor;
 }
 
-function changeColor(colorState) {
-  canvasEl.removeEventListener('mouseover', fillPixel);
-
-  if ((colorState = 'random')) {
-    canvasEl.addEventListener('mouseover', fillRandom);
-  } else if ((colorState = 'grayscale')) {
-    canvasEl.addEventListener('mouseover', fillGray);
-  } else if ((colorState = 'specified')) {
-    canvasEl.addEventListener('mouseover', fillSpecified);
-  }
+function getWarmColor(e) {
+  let color1 = Math.floor(Math.random() * 255);
+  let color2 = Math.floor(Math.random() * 100);
+  return `rgb(${color1}, ${color2}, 0)`;
 }
 
-function fillRandom() {
-  if (!brushState) return;
+function getCoolColor(e) {
+  let color1 = Math.floor(Math.random() * 200);
+  let color2 = Math.floor(Math.random() * 255);
+  return `rgb(0, ${color1}, ${color2})`;
+}
 
-  const pixel = e.target;
-  pixel.style.backgroundColor = getRandomColor();
+function getGrayColor(e) {
+  let alpha = Math.random();
+  return `rgba(0, 0, 0, ${alpha})`;
+}
+
+function makeActive(target) {
+  const childrenArr = Array.from(colorsContainer.children);
+  childrenArr.forEach((btn) => btn.classList.remove('btn-active'));
+  target.classList.add('btn-active');
 }
 
 // Event listeners
 canvasEl.addEventListener('mouseover', fillPixel);
 
-btnRandom.addEventListener('click', changeColor);
-
-btnEraseAll.addEventListener('click', eraseAllFills);
 btnEraseBrush.addEventListener('click', toggleErase);
+btnEraseAll.addEventListener('click', eraseAllFills);
 btnToggle.addEventListener('click', toggleDrawing);
 window.addEventListener('keydown', toggleDrawing);
 btnNew.addEventListener('click', displayModal);
 btnSlider.addEventListener('click', makeCanvas);
 btnQuery.addEventListener('click', makeCanvas);
 overlayDiv.addEventListener('click', makeCanvas);
+
+colorsContainer.addEventListener('click', changeColor);
+btnRandom.addEventListener('click', getRandomColor);
+btnWarm.addEventListener('click', getWarmColor);
+btnCool.addEventListener('click', getCoolColor);
+btnGray.addEventListener('click', getGrayColor);
 
 // Initialize game
 init();
